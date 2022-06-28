1e9933ee1bb5a4e0f6c5138d96473beacbb110a1
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-06-28 10:03:25 +0900

    add: global guard setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { UsersAuthGuard } from './users/users-auth/users-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalGuards(new UsersAuthGuard());
   await app.listen(3000);
 }
 bootstrap();
