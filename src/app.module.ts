c8a8277a543cb5c962c0693a9e87ba292264181f
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-06-28 10:05:49 +0900

    add: guard to inject another provider,  declare custom provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersAuthGuard } from './users/users-auth/users-auth.guard';
 import { UsersAuthModule } from './users/users-auth/users-auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule, UsersAuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: UsersAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
